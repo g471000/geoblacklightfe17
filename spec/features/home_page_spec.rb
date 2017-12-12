82d2caac1ec022b791c94f3b9765402049dd0496
haigouseal <whtseal@gmail.com>
2017-12-11 22:40:40 -0500

    update search history

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -8,7 +8,7 @@ feature 'Home page', js: true do # use js: true for tests which require js, but 
     expect(page).not_to have_css '#search-navbar'
     within '.jumbotron' do
       expect(page).to have_css 'h2', text: 'Global Event and Trend'
-      expect(page).to have_css 'h3', text: 'Find the maps and data you need'
+      expect(page).to have_css 'h3', text: 'Web archive of urgent global challenge events and initiatives'
       expect(page).to have_css 'form.search-query-form'
     end
   end
